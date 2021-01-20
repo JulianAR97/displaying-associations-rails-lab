a93966cc8fa5d4f39a509cea302907a51e546061
Julian <julianrosenthal@yahoo.com>
2021-01-19 19:32:45 -0700

    commit tree b430930eb504d047bd39bd2925334b6b8e9b6913

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   belongs_to :artist
+
+  def artist_name
+    artist.name
+  end
+
+  def format_song
+    "#{artist_name.titlecase} - #{title.titlecase}"
+  end
 end
